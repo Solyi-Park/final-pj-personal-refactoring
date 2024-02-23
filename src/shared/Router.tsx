8447355b1bee1e72a62a72c430bf4436a644f68d
Solyi Park <solyipark@Solyiui-MacBookPro.local>
2024-02-23 17:48:28 +0900

    modify: router lazy loading 적용

--- src/shared/Router.tsx
+++ src/shared/Router.tsx
@@ -10,24 +10,35 @@ import Home from 'pages/home/Home';
 import MyPage from 'pages/mypage/MyPage';
 import Write from 'pages/write/Write';
 import ProtectedRoute from './ProtectedRoute';
+import React, { Suspense } from 'react';
+const LazyHome = React.lazy(() => import('pages/home/Home'));
+const LazyAbout = React.lazy(() => import('pages/about/About'));
+const LazyAuth = React.lazy(() => import('pages/auth/Auth'));
+const LazyByMango = React.lazy(() => import('pages/bymango/ByMango'));
+const LazyCommunity = React.lazy(() => import('pages/community/Community'));
+const LazyDetail = React.lazy(() => import('pages/detail/Detail'));
+const LazyMypage = React.lazy(() => import('pages/mypage/MyPage'));
+const LazyWrite = React.lazy(() => import('pages/write/Write'));
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Layout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/community" element={<Community />} />
-          <Route path="/bymango" element={<ByMango />} />
-          <Route path="/auth" element={<Auth />} />
-          <Route path="/detail/:id" element={<Detail />} />
-          <Route path="/about" element={<About />} />
-          <Route path="*" element={<Navigate replace to="/" />} />
-          <Route element={<ProtectedRoute />}>
-            <Route path="/mypage" element={<MyPage />} />
-            <Route path="/write" element={<Write key={new Date().getTime()} />} />
-          </Route>
-        </Routes>
+        <Suspense>
+          <Routes>
+            <Route path="/" element={<LazyHome />} />
+            <Route path="/community" element={<LazyCommunity />} />
+            <Route path="/bymango" element={<LazyByMango />} />
+            <Route path="/auth" element={<LazyAuth />} />
+            <Route path="/detail/:id" element={<LazyDetail />} />
+            <Route path="/about" element={<LazyAbout />} />
+            <Route path="*" element={<Navigate replace to="/" />} />
+            <Route element={<ProtectedRoute />}>
+              <Route path="/mypage" element={<LazyMypage />} />
+              <Route path="/write" element={<LazyWrite key={new Date().getTime()} />} />
+            </Route>
+          </Routes>
+        </Suspense>
       </Layout>
       <ReactQueryDevtools />
     </BrowserRouter>
